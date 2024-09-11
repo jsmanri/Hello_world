24f73ca0fe1ba062999f585e1a14ccb68b9fbdf1
Juan Manrique <jsmanriquemesa14@gmail.com>
2024-09-11 16:42:24 -0500

    Agregue un archivo

--- Prueba.py
+++ Prueba.py
@@ -1,5 +1,17 @@
 import time
-print("Hello world")
+
+print("Hola") # Esta linea imprime en consola
+print("Hello world \n")
 print("Segundo reglon")
-time.sleep(10)
+time.sleep(3)
+
 print("Yashido")
+
+print(type("Hello")) # Esta funcion dice en consola los tipos de datos que son
+print(type(4))
+print(type(5.5))
+"""
+Fue
+el
+Yashido  
+"""
